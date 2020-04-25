cff57ecb8c1c61a5e408d3212634dee6fd69181a
Dries De Rudder <dries.derudder@scaledaccess.com>
2020-04-25 16:52:36 +0200

    add manifest.py

---
+++ project_type/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# By DDR
+
+{
+    'name': 'Project Types',
+    'version': '1.0',
+    'website': 'https://www.houtbouw-derudder.be',
+    'category': 'Operations/Project',
+    'sequence': 10,
+    'summary': 'Define different types of projects',
+    'depends': [
+        'project',
+    ],
+    'description': "This module allows to define a type on a project.",
+    'data': [
+        # 'security/project_security.xml',
+        # 'security/ir.model.access.csv',
+        # 'report/project_report_views.xml',
+        # 'views/analytic_views.xml',
+        # 'views/digest_views.xml',
+        # 'views/rating_views.xml',
+        # 'views/project_views.xml',
+        # 'views/res_partner_views.xml',
+        # 'views/res_config_settings_views.xml',
+        # 'views/mail_activity_views.xml',
+        # 'views/project_assets.xml',
+        # 'views/project_portal_templates.xml',
+        # 'views/project_rating_templates.xml',
+        # 'data/digest_data.xml',
+        # 'data/project_mail_template_data.xml',
+        # 'data/project_data.xml',
+    ],
+    'demo': [
+		# 'data/project_demo.xml'
+		],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
